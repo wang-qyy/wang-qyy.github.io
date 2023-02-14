b53f7692d6f3449251c82a7ae291edea3f5bf827
wangqiyue <wang_qyy@163.com>
2023-02-14 17:35:54 +0800

    feat: pwa test

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,12 @@
-self.addEventListener("install", function (e) {})
+self.addEventListener('install', function (e) {
+    console.log('install success')
+});
 
-self.addEventListener("fetch", function (e) {})
+self.addEventListener('fetch', function (e) {
+    console.log(e.request.url);
+    // e.respondWith(
+    //   caches.match(e.request).then(function(response) {
+    //     return response || fetch(e.request);
+    //   })
+    // );
+});
