a5e303e37d37490418edffaab95745a1d1e1c8c5
wangqiyue <wang_qyy@163.com>
2023-02-14 18:14:35 +0800

    feat: pwa demo

--- a2hs/index.js
+++ a2hs/index.js
@@ -1,22 +1,11 @@
-const images = ['fox1', 'fox2', 'fox3', 'fox4'];
-const imgElem = document.querySelector('img');
-
-function randomValueFromArray(array) {
-  const randomNo = Math.floor(Math.random() * array.length);
-  return array[randomNo];
-}
-
-setInterval(() => {
-  const randomChoice = randomValueFromArray(images);
-  imgElem.src = `images/${randomChoice}.jpg`;
-}, 2000);
-
 // Register service worker to control making site work offline
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/pwa-examples/a2hs/sw.js')
-    .then(() => { console.log('Service Worker Registered'); });
+    .then(() => {
+      console.log('Service Worker Registered');
+    });
 }
 
 // Code to handle install prompt on desktop
